d412110a08a4fb2414d0b5a48f9656b37b8f498a
vparon <vpa.2000.szolgalat@gmail.com>
2021-05-25 18:01:31 +0200

    AppComp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AppComponent} from './app.component';
+import {ListComponent} from './list/list.component';
+import {LoginComponent} from './login/login.component';
+import {LoginGuard} from './Guard/login.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'main', component: AppComponent },
+  { path: '', redirectTo: '/main', pathMatch: 'full' },
+
+  {
+    path: 'list',
+    component: ListComponent,
+    canActivate: [LoginGuard]
+  },
+  {
+    path: 'details/:id',
+    component: AppComponent,
+    canActivate: [LoginGuard]
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+
+  },
+
+  { path: '**', component: AppComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSelectModule } from '@angular/material/select';
+import { MatChipsModule } from '@angular/material/chips';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list/list.component';
+import { LoginComponent } from './login/login.component';
+import { RatesComponent } from './rates/rates.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    LoginComponent,
+    RatesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
